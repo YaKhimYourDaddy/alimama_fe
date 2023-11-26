5d7020ae4e860ca2a1f954af8b2396c062cf7856
YaKhimYourDaddy <khiem140302@gmail.com>
2023-11-26 18:08:47 +0700

    57

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
+
 
 const createUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
@@ -17,6 +19,8 @@ const createUser = asyncHandler(async (req, res) => {
 
     try {
         await newUser.save();
+        createToken(res, newUser._id);
+
         res.status(201).json({
             _id: newUser._id,
             username: newUser.username,
@@ -29,4 +33,94 @@ const createUser = asyncHandler(async (req, res) => {
     }
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        const isPasswordValid = await bcrypt.compare(
+            password,
+            existingUser.password
+        );
+
+        if (isPasswordValid) {
+            createToken(res, existingUser._id);
+
+            res.status(201).json({
+                _id: existingUser._id,
+                username: existingUser.username,
+                email: existingUser.email,
+                isAdmin: existingUser.isAdmin,
+            });
+            return;
+        }
+    }
+});
+
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+    res.cookie("jwt", "", {
+        httyOnly: true,
+        expires: new Date(0),
+    });
+
+    res.status(200).json({ message: "Logged out successfully" });
+});
+
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+});
+
+
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found.");
+    }
+});
+
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            user.password = hashedPassword;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+});
+
+
+export {
+    createUser,
+    loginUser,
+    logoutCurrentUser,
+    getAllUsers,
+    getCurrentUserProfile,
+    updateCurrentUserProfile,
+};
